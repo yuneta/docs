ddc0db76625cefdba56c4a4b6e2a7caf09260bbf
niyamaka <ginsmar@gmail.com>
2023-05-18 11:20:21 +0200

    fix to last kubuntu version

--- yuneta/_static/install-yuneta-dependencies.sh
+++ yuneta/_static/install-yuneta-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  Exit immediately if a command exits with a non-zero status.
-set -e
+#set -e
 
 
 ################################################
@@ -12,8 +12,10 @@ if [ -f "/usr/bin/apt-get" ]; then
 #   With apt-get
 #
     sudo apt -y install vim rsync tree sudo ssh curl fail2ban
-    sudo apt -y install autotools-dev automake autogen libtool debmake cmake gettext mercurial git psmisc liblzma-dev zlib1g-dev libpcre3-dev libcurl4-openssl-dev libldap2-dev libidn11-dev libidn2-0-dev librtmp-dev libprocps-dev uuid-dev libarchive-dev libpq-dev can-utils libsocketcan2 libsocketcan-dev
-    sudo apt -y install clang libc++-dev libc++abi-dev
+    sudo apt -y install autotools-dev automake autogen libtool debmake cmake gettext mercurial git psmisc liblzma-dev zlib1g-dev libpcre3-dev libcurl4-openssl-dev libldap2-dev libidn11-dev libidn2-0-dev librtmp-dev uuid-dev libarchive-dev libpq-dev can-utils libsocketcan2 libsocketcan-dev
+    sudo apt -y install clang libc++-dev libc++abi-dev libunwind-dev
+    sudo apt - install libprocps-dev
+    sudo apt - install libproc2-dev
 
 elif [ -f "/usr/bin/yum" ]; then
 #
